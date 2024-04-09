6aa58bbc5e2785705b49976b3d7ffb68fc7ea21a
Chris Renshaw <osm0sis@outlook.com>
2024-04-09 08:24:33 -0300

    Indicate scripts-only mode in module description

--- module/customize.sh
+++ module/customize.sh
@@ -2,6 +2,7 @@
 if [ -f /data/adb/modules/playintegrityfix/scripts-only-mode ]; then
     ui_print "! Installing global scripts only; Zygisk attestation fallback and device spoofing disabled"
     touch $MODPATH/scripts-only-mode
+    sed 's/\(description=\)\(.*\)/\1[Scripts-only mode] \2/' $MODPATH/module.prop
     rm -rf $MODPATH/classes.dex $MODPATH/common_setup.sh $MODPATH/custom.pif.json \
         $MODPATH/example.app_replace.list $MODPATH/example.pif.json $MODPATH/migrate.sh $MODPATH/zygisk \
         /data/adb/modules/playintegrityfix/custom.app_replace.list \
