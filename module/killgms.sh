4d116c6a4211308bf2c6dfb153cb6cae61fbc9d1
Chris Renshaw <osm0sis@outlook.com>
2024-05-06 09:59:41 -0300

    Add DroidGuard process killing script

---
+++ module/killgms.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+# killgms.sh by osm0sis @ xda-developers
+#
+# Kill the Google Play services DroidGuard process
+# (com.google.android.gms.unstable)
+
+if [ "$USER" != "root" -o "$(whoami 2>/dev/null)" != "root" ]; then
+  echo "killgms: need root permissions";
+  exit 1;
+fi;
+
+killall -v com.google.android.gms.unstable;
