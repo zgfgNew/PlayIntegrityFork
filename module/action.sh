da0fad725ab6093586b5c6a8553ace5dc2b558da
Chris Renshaw <osm0sis@outlook.com>
2024-12-29 23:59:50 -0400

    MMRL (3rd party module manager) doesn't auto-close Actions so exclude it

--- module/action.sh
+++ module/action.sh
@@ -9,7 +9,7 @@ sh $MODPATH/autopif2.sh || exit 1
 echo -e "\nDone!"
 
 # warn since KernelSU/APatch's implementation automatically closes if successful
-if [ "$KSU" = "true" -o "$APATCH" = "true" ]; then
+if [ "$KSU" = "true" -o "$APATCH" = "true" ] && [ "$MMRL" != "true" ]; then
     echo -e "\nClosing dialog in 20 seconds ..."
     sleep 20
 fi
