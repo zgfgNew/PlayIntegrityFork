8805c3736d0308813d617ceb778ce903cc8770b3
Chris Renshaw <osm0sis@outlook.com>
2025-02-07 00:19:10 -0400

    Add com.android.vending to DenyList changes

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -2,15 +2,17 @@ MODPATH="${0%/*}"
 . $MODPATH/common_func.sh
 
 if [ -d "$MODPATH/zygisk" ]; then
-    # Remove Play Services from Magisk DenyList when set to Enforce in normal mode
+    # Remove Play Services and Play Store from Magisk DenyList when set to Enforce in normal mode
     if magisk --denylist status; then
         magisk --denylist rm com.google.android.gms
+        magisk --denylist rm com.android.vending
     fi
     # Run common tasks for installation and boot-time
     . $MODPATH/common_setup.sh
 else
-    # Add Play Services DroidGuard process to Magisk DenyList for better results in scripts-only mode
+    # Add Play Services DroidGuard and Play Store processes to Magisk DenyList for better results in scripts-only mode
     magisk --denylist add com.google.android.gms com.google.android.gms.unstable
+    magisk --denylist add com.android.vending
 fi
 
 # Conditional early sensitive properties
